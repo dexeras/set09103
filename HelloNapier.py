feba362095b3d1788ca6a4a3d8c1b82a8771df9a
dexeras <40286996@napier.ac.uk>
2016-09-29 14:49:24 +0000

    added a few routes related to chapter 4 of the workbook.

--- HelloNapier.py
+++ HelloNapier.py
@@ -1,9 +1,10 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  print request.method, request.path, request.form
+  return request.method+request.path+"Hello World!"
 
 @app.route('/apple')
 def static_example_img():
@@ -11,6 +12,52 @@ def static_example_img():
   url = url_for('static', filename='apple.jpg')
   end='">'
   return start+url+end, 200
+
+@app.route("/account/", methods=['POST','GET'])
+def account():
+  if request.method=='POST':
+    print request.form
+    name = request.form['name']
+    return request.method+request.path+"Hello %s" % name
+  else:
+    page='''
+    <html><body>
+      <form action="" method="post" name="form">
+        <label for="name">Name : </label>
+        <input type="text" name = "name" id="name"/>
+        <input type="submit" nqme="submit" id="submit"/>
+      </form>
+    </body></html>'''
+    return request.method+request.path+page
+
+@app.route("/hello/")
+def helloname():
+  name = request.args.get('name','')
+  if name == '':
+    return'''Hello stranger, give me your name by typing it as an encoded
+    parameter in the URL ;)'''
+  else:
+    return "Hello %s" % name
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first,second):
+  return str(first+second)
+
+@app.route("/upload/", methods=['POST','GET'])
+def upload():
+  if request.method=="POST":
+    f=request.files['datafile']
+    f.save('static/uploads/file.png')
+    return "File uploaded"
+  else:
+    page='''
+    <html><body>
+    <form action="" method="post" name="form" enctype="multipart/form-data">
+    <input type="file" name="datafile" />
+    <input type="submit" name="submit" id="submit"/>
+    </form></body></html>
+    '''
+    return page, 200
 
 @app.errorhandler(404)
 def page_not_found(error):
